fd37873ee6f8e14a56cad1e70b2f24a02dc5dd1e
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-12-16 21:46:46 -0500

    commit tree d14436327527c5e514244072b58726b438629555

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = event => {
+    // console.log(event.target.value, "handling change")
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let remChar = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={this.handleChange}
+          value={this.state.message}
+        />
+        <p> Remaining Characters: {remChar}</p>
       </div>
     );
   }
